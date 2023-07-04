d5f87f0ef653e2fea736d0da4888fea4c8fe9ae0
jsandovalg <jorge@stakeordie.com>
2023-07-04 15:44:35 -0700

    fix(SNW-210): Remove secret_agents collection

--- src/api/secret-agent/routes/secret-agent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * secret-agent router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::secret-agent.secret-agent');
--- src/api/secret-agent/services/secret-agent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * secret-agent service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::secret-agent.secret-agent');
