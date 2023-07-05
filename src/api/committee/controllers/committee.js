204f9289e48112450579163a84f5dc4167b3eacd
jsandovalg <jorge@stakeordie.com>
2023-07-04 18:03:12 -0700

    fix(SNW-210): Remove committees collection

--- src/api/committee/controllers/committee.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  committee controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::committee.committee');
