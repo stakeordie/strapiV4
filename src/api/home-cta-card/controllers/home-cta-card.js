af0a1fb8773f06ef04a07783e4df7029a434603d
jsandovalg <jorge@stakeordie.com>
2023-07-07 14:31:56 -0700

    fix(SNW-212): Remove from strapi collection type Home CTA Cards

--- src/api/home-cta-card/controllers/home-cta-card.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  home-cta-card controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-cta-card.home-cta-card');
--- src/api/home-cta-card/routes/home-cta-card.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-cta-card router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::home-cta-card.home-cta-card');
