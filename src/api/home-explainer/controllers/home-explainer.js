0bbb70566a8c7a736c15936bd8e61864cf05a9f8
jsandovalg <81380326+jsandovalg@users.noreply.github.com>
2023-07-10 15:34:23 -0700

    fix(SNW-213): Remove home explainer collection (#62)

--- src/api/home-explainer/controllers/home-explainer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  home-explainer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-explainer.home-explainer');
--- src/api/home-explainer/routes/home-explainer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-explainer router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::home-explainer.home-explainer');
