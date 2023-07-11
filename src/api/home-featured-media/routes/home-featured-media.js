8a0a8f3d530fb60ed6c457ad5a2fe19f8f4e9cea
jsandovalg <81380326+jsandovalg@users.noreply.github.com>
2023-07-11 10:18:04 -0700

    fix(SNW-217): Remove collection type home - featured media (#64)

--- src/api/home-featured-media/routes/home-featured-media.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-featured-media router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::home-featured-media.home-featured-media');
