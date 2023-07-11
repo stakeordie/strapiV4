c7d603c7591ce8327b4b7bc98beaf711a599e0d7
jsandovalg <jorge@stakeordie.com>
2023-07-11 09:21:18 -0700

    Remove Single Type Home-Hero

--- src/api/home-hero/controllers/home-hero.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  home-hero controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-hero.home-hero');
--- src/api/home-hero/routes/home-hero.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-hero router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::home-hero.home-hero');
