1357d1f41b93878ca4a92bf0fe123e716a12c75b
jsandovalg <81380326+jsandovalg@users.noreply.github.com>
2023-07-11 09:28:12 -0700

    Remove Single Type Home-Hero (#63)

--- src/api/home-hero/services/home-hero.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-hero service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::home-hero.home-hero');
