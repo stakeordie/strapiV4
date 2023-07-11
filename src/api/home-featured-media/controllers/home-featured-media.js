64db45983e6d3b30a775bfc3c126f9b8363c9dbb
jsandovalg <jorge@stakeordie.com>
2023-07-11 10:15:19 -0700

    fix(SNW-217): Remove collection type home - featured media

--- src/api/home-featured-media/controllers/home-featured-media.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  home-featured-media controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-featured-media.home-featured-media');
--- src/api/home-featured-media/services/home-featured-media.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-featured-media service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::home-featured-media.home-featured-media');
